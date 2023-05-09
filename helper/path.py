984f669a1b30f757e1da2d5245cc4b7695aed01d
hafif <hafifmuhammad.kalbecrm@gmail.com>
2023-05-09 13:36:21 +0800

    fix: prod and dev stage sql queries folder

--- helper/path.py
+++ helper/path.py
@@ -1,5 +1,9 @@
 import os
+from dotenv import load_dotenv
+
+# load the env
+load_dotenv()
 
 ABSOLUTE_PATH = os.path.dirname(__file__)
-RELATIVE_PATH = "sql/"
+RELATIVE_PATH = "sql/dev" if os.getenv("STAGE") == "development" else "sql/prod"
 FULL_PATH = os.path.join(ABSOLUTE_PATH, RELATIVE_PATH)
